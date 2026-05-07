e6ee9808c49126beee8360c3f2acb0a894f0122a
bxzing <62268923+r00kieL@users.noreply.github.com>
2026-05-07 16:50:06 +0800

    feat: 引入tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+
 import { dialogue } from "./dialogues";
+import "./App.css";
 
 export default function App() {
   const [dialogueId, setDialogueId] = useState(1);
@@ -7,38 +9,45 @@ export default function App() {
 
   return (
     <>
-      {/* 对话框 */}
-      <div>
-        <span>
-          <b>{dialogue[dialogueId].speaker}</b>
-        </span>
-        <span> - </span>
-        {dialogue[dialogueId].textList
-          .slice(0, currentTextId + 1)
-          .map(({ text, key }) => (
-            <p key={key}>{text}</p>
-          ))}
-      </div>
+      <div className="flex">
+        {/* 游戏内容，暂无 */}
+        <div className="flex-6"></div>
 
-      {/* 下一步的分支按钮 */}
-      <div>
-        {currentTextId + 1 === dialogue[dialogueId].textList.length ? (
-          dialogue[dialogueId].options.map(({ label, key, nextId }) => (
-            <button
-              key={key}
-              onClick={() => {
-                setDialogueId(nextId);
-                setCurrentTextId(0);
-              }}
-            >
-              {label}
-            </button>
-          ))
-        ) : (
-          <button onClick={() => setCurrentTextId(currentTextId + 1)}>
-            继续
-          </button>
-        )}
+        <div className="flex-4 flex flex-col">
+          {/* 对话框 */}
+          <div>
+            <span>
+              <b>{dialogue[dialogueId].speaker}</b>
+            </span>
+            <span> - </span>
+            {dialogue[dialogueId].textList
+              .slice(0, currentTextId + 1)
+              .map(({ text, key }) => (
+                <p key={key}>{text}</p>
+              ))}
+          </div>
+
+          {/* 下一步的分支按钮 */}
+          <div>
+            {currentTextId + 1 === dialogue[dialogueId].textList.length ? (
+              dialogue[dialogueId].options.map(({ label, key, nextId }) => (
+                <button
+                  key={key}
+                  onClick={() => {
+                    setDialogueId(nextId);
+                    setCurrentTextId(0);
+                  }}
+                >
+                  {label}
+                </button>
+              ))
+            ) : (
+              <button onClick={() => setCurrentTextId(currentTextId + 1)}>
+                继续
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
